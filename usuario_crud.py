f5e31ff9fe5a33a0e903111d423f7d9bb4ecdf82
LucasFrancaRegistro <lucascumuru@gmail.com>
2023-06-06 19:12:24 -0300

    crud usuario

--- usuario_crud.py
+++ usuario_crud.py
@@ -1,122 +1,126 @@
 
 import pickle
-from cassandra_connect import cluster
+import uuid
+from tabelas import cluster
 
 session = cluster.connect()
 
 
-def createEndereco(email):
-    rua =  input('Nome da rua: ')
-    bairro = input('Nome do bairro: ')
-    numero = input('Numero da residencia: ')
-    complemento = input('Complemento (opcional): ')
-    session.execute("INSERT INTO endereco (id, email_residente, rua, bairro, numero, complemento) VALUES (%s, %s, %s, %s, %s, %s)", (uuid.uuid1(), email, rua, bairro, numero, complemento))
+def insert_usuario(session, sobrenome, email, nome, endereco):
+    id = uuid.uuid4()
+    session.execute("INSERT INTO usuarios (id, sobrenome, email, nome, endereco) VALUES (%s,%s,%s,%s,%s)", [id, sobrenome, email, nome, endereco])
 
-def updateEndereco(usuario):
+def insert_favoritos(session,email,produto):
+  id = uuid.uuid4()
+  session.execute("INSERT INTO favoritos (id, email, produto) VALUES (%s,%s,%s)", [id, email,produto])
 
-    enderecos = session.execute(" SELECT * FROM endereco WHERE email_residente=%s", (usuario.email))
-    print('''O que deseja fazer?
-    1:  Atualizar endereço
-    2:  Adicionar endereço
-    3:  Deletar endereço''')
-    escolha = input("Escolha uma opção: ")
-    if escolha == '1':
-        for index in range(len(enderecos)):
-            print(str(index) + ':' + str(enderecos[index]))
-        enderecoVelho = int(input('Escolha o endereço a atualizar: '))
-        enderecoNovo = createEndereco(usuario.email)
-        enderecos[enderecoVelho] = enderecoNovo
-    elif escolha == '2':
-        createEndereco(usuario.email)   
-    elif escolha == '3':
-        for index in range(len(enderecos)):
-            print(str(index) + ':' + str(enderecos[index]))
-        session.execute(" DELETE FROM endereco WHERE id=", (enderecos[index].id))
-    
+def find_usuarios():
+  result = session.execute("SELECT * FROM usuarios")
+  return result
 
+def find_favoritos():
+  result = session.execute("SELECT * FROM favoritos;")
+  return result
 
-
-
-def insertUsuario():
-    nome = input('Nome do usuario: ')
-    email = input('Email do usuario: ')
-    cpf = input('Cpf do usuario: ')
-    while True:
-        createEndereco(email)
-        resposta = input('Quer adicionar outro endereço? (y/n) ')
-        if resposta == 'n':
-            break
-    session.execute(" INSERT INTO usuario (cpf, nome, email) VALUES (%s, %s, %s)", (cpf, nome, email))
-
-def sortUsuario():
-    docs = session.execute(" SELECT * FROM usuario")
-    usuarios = []
-    for obj in docs:
-        enderecos, favoritos = [], []
-        for endereco in session.execute(" SELECT * FROM endereco WHERE email_residente=%s", (obj.email)):
-            enderecos.append(endereco)
-        for favorito in session.execute(" SELECT * FROM favoritos WHERE email_usuario=%s", (obj.email)):
-            favoritos.append(session.execute(" SELECT * FROM produtos WHERE id_produto=%s", (favorito.id_produto)))
-        usuario = {
-            usuario: obj
-            enderecos: enderecos
-            favoritos: favoritos
-        }
-        objetos.append(obj)
-    return objetos
-
-def updateUsuario():
-    from compras_crud import search
-    global db
-    col = db.usuario
-    usuarios = search(sortUsuario())
-    usuario = usuarios[int(input("Escolha o usuario que deseja editar: "))]
-    print('''O que deseja editar? 
-    Nome
-    Email
-    Cpf
-    Endereço
-    Favoritos''')
-    escolha = input("Escreva a sua opção: ").lower()
-    if escolha.lower() == "endereço":
-        updateEndereco(usuario)
-    elif escolha.lower() == "favoritos":
-        updateFavorito(usuario)
+def delete_usuario(session, email):
+    id_result = session.execute("SELECT id FROM usuarios WHERE email = %s", [email])
+    id = id_result.one().id if id_result else None
+    if id:
+        prepared = session.prepare("DELETE FROM usuarios WHERE id = ?")
+        session.execute(prepared, [id])
     else:
-        valor = input("valor novo ")
-        toUpdate = { "$set": { escolha: valor} }
-        query = { "_id": usuario["_id"]}
-        col.update_one(query, toUpdate)
-    
+        print("Usuário não encontrado.")
 
-def deleteUsuario():
-    usuarios = sortUsuario()
-    for index in usuarios:
-        print(str(index) + ":  " + str(usuarios[index]))
-    usuario = usuarios[int(input("Escolha o usuario a deletar: "))]
-    session.execute("DELETE FROM usuario WHERE email=s%", (usuario.email))
-    session.execute("DELETE FROM endereco WHERE email_residente=s%", (usuario.email))
-    session.execute("DELETE FROM favoritos WHERE email_usuario=s%", (usuario.email))
+def deletar_favorito(session, email):
+    nome_result = session.execute("SELECT nome FROM usuarios WHERE email = %s", [email])
+    nome = nome_result.one().nome if nome_result else None
+    if nome:
+        print(f"Cliente: {nome}")
+        favoritos = session.execute("SELECT * FROM favoritos WHERE email = %s", [email])
+        posicao = 1
+        for favorito in favoritos:
+            preco = session.execute("SELECT preco FROM produtos WHERE nome = %s", [favorito.produto]).one().preco
+            nome = favorito.produto
+            print(f"0{posicao} - Produto: {nome}, Preço: R${str(preco).replace('.', ',')}")
+            posicao += 1
+        chave = True
+        while chave:
+            produtoNome = input("Produto a excluir: ")
+            chave = (input("Deseja excluir outro produto? (s/n): ") == 's')
+            id_result = session.execute("SELECT id FROM favoritos WHERE email = %s AND produto = %s", [email, produtoNome])
+            id = id_result.one().id if id_result else None
+            if id:
+                prepared = session.prepare("DELETE FROM favoritos WHERE id = ?")
+                session.execute(prepared, [id])
+            else:
+                print("Favorito não encontrada.")
+    else:
+        print("Usuário não encontrado.")
 
+def adastro_usuario():
+    nome = input("Nome: ")
+    sobrenome = input("Sobrenome: ")
+    email = input("Email: ")
+    print("Endereços")
+    enderecos = []
+    adicao = True
+    while adicao:
+        cep = input("CEP: ")
+        numero = input("Número: ")
+        enderecos.append({"cep": cep,"numero": numero})
+        adicao = (input("Deseja adicionar outro endereço? (s/n): ") == 's')
+    insert_usuario(session, sobrenome, email, nome, enderecos)
 
+def pega_usuarios():
+    usuarios = find_usuarios()
+    for usuario in usuarios:
+        print("Nome: " + usuario.nome)
+        print("Sobrenome: " + usuario.sobrenome)
+        print("Email: " + usuario.email)
+        posicao = 1
+        if usuario.endereco is not None:
+            print("Endereços: ")
+            for endereco in usuario.endereco:
+                cep = endereco["cep"]
+                numero = endereco["numero"]
+                print(f"0{posicao} - CEP: {cep}, Número: {numero}")
+                posicao += 1
+        print("Favoritos: ")
+        pega_favoritos(usuario.email)
 
-def updateFavorito(usuario):
-    from compras_crud import search
-    from produto_crud import sortProduto
-    global db
-    col = db.usuario
-    favoritos = usuario["favoritos"]
-    print('''O que deseja fazer?
-    1:  Adicionar aos favoritos
-    2:  Remover dos favoritos''')
-    escolha = input('Escolha uma opção: ')
-    if escolha == '1':
-        produtos = search(sortProduto())
-        produto = (produtos[int(input("escolha o produto que quer adicionar: "))])
-        session.execute(" INSERT INTO favoritos VALUES ")
-    elif escolha == '2':
-        for index in range(len(favoritos)):
-            print(str(index) + ':' + str(favoritos[index]))
-        favorito = favoritos[(int(input("Escolha o produto para remover: ")))]
-        session.execute("DELETE FROM favoritos WHERE id=s%", (favorito.id))
+def pega_favoritos(email):
+        posicao = 1
+        favoritos = session.execute("SELECT * FROM favoritos WHERE email = %s", [email])
+        for favorito in favoritos:
+            preco = session.execute("SELECT preco FROM produtos WHERE nome = %s", [favorito.produto]).one().preco
+            nome = favorito.produto
+            print(f"0{posicao} - Produto: {nome}, Preço: R${str(preco).replace('.', ',')}")
+            posicao += 1
 
+def atualizar_usuario():
+    email = input("Email do usuário a atualizar: ")
+    row = session.execute("SELECT id FROM usuarios WHERE email = %s", [email]).one()
+    if not row:
+        print("Usuário não encontrado.")
+        return 
+    user_id = row.id
+    print("Quais campos deseja atualizar?")
+    print("01 - Nome")
+    print("02 - Sobrenome")
+    print("03 - Email")
+    campos = input("Quais campos? (exemplo: 01,02,03): ")
+    campos = campos.split(",")
+    for campo in campos:
+        campo = int(campo)
+        if campo == 1:
+            nome = input("Novo nome: ")
+            session.execute("UPDATE usuarios SET nome = %s WHERE id = %s", [nome, user_id])
+        elif campo == 2:
+            sobrenome = input("Novo sobrenome: ")
+            session.execute("UPDATE usuarios SET sobrenome = %s WHERE id = %s", [sobrenome, user_id])
+        elif campo == 3:
+            novo_email = input("Novo email: ")
+            session.execute("UPDATE usuarios SET email = %s WHERE id = %s", [novo_email, user_id])
+
+def cadastrar_favoritos():
+    cadastrar_itens("favoritos", "Nome do produto favoritado: ", "Deseja adicionar outro favorito? (s/n): ")
